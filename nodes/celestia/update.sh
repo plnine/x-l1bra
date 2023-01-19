6bb7eb82f87d04cdedf76c40e5aa9541e7f879bb
plnine <pl9-fund@pm.me>
2023-01-20 02:24:28 +0300

    Update update.sh

--- nodes/celestia/update.sh
+++ nodes/celestia/update.sh
@@ -11,9 +11,7 @@ echo $(cyanprint    'Установлена версия')
 celestia-appd version
 read $var1
 
-var2="0.11.0"
-
-
+$var2="0.11.0"
 
 if $var1 == $var2; then
 echo "Обновление не требуется"
@@ -21,41 +19,3 @@ elif $var1 != $var2;
 mainmenu
 fi
 
-mainmenu() {
-    echo -ne "
-$(redprint    'Желаете обновить ?')
-$(greenprint   '1) Да')
-$(redprint     '2) Нет')
-$(cyanprint 'Введите цифру:') "
-   read -r ans
-    case $ans in
-    1)
-        yes
-        ;;
-    2)
-        no
-        ;;
-   
-    *)
-        clear
-	printLogo
-	printRed  =======================
-	echo $(redprint '==') $(cyanprint 'CELESTIA') $(redprint '==') $(yellowprint '*****') $(redprint '==')
-	printRed  =======================
-	echo $(redprint 'Неверный запрос !')
-        ;;
-    esac
-}
-
-yes(){
-clear
-printLogo
-printRed  =======================================================================
-mainmenu
-}
-
-no(){
-        source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/celestia/main.sh)
-}
-
-mainmenu
