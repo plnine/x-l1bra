18dd985abedc41cfbccd6c23d07adf128fe71b6f
plnine <pl9-fund@pm.me>
2023-01-19 14:33:30 +0300

    update

---
+++ scripts/x-l1bra.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+#X-l1bra  
+clear && source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/common.sh)
+printLogo
+printRed  =======================================================================
+
+mainmenu() {
+    echo -ne "
+$(cyanprint    'Выберите ноду !')
+$(blueprint    '1)') Celestia
+$(redprint     '0)') Exit
+Введите цифру:  "
+    read -r ans
+    case $ans in
+    1)
+        celestia
+        ;;
+    0)
+        echo $(cyanprint '"Bye bye."')
+        exit
+        ;;
+    *)
+        printLogo
+        printRed  ======================================================================= 
+        mainmenu
+        ;;
+    esac
+}
+
+celestia(){
+source <(curl -s https://raw.githubusercontent.com/plnine/development/main/x-l1bra/nodes/celestia/main.sh)
+}
+
+
+mainmenu
