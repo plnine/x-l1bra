db89ae529ca5cf84c4bb8cff34f029995cbd3d5c
plnine <pl9-fund@pm.me>
2023-01-20 10:10:59 +0300

    Update update.sh

--- nodes/celestia/update.sh
+++ nodes/celestia/update.sh
@@ -14,8 +14,8 @@ var2=0.11.0
 
 if (( "$var1" -eq "$var2" )); then
 echo "OK"
-elif $var1 != $var2; then
-echo upfate
+elif (( "$var1" != "$var2" )); then
+echo "upfate"
 fi
 
 #if [ "$var1" == "$var2" ]; then
