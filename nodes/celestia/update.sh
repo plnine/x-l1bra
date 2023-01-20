40df3a94d4aa4f18028dbf56303c115c2013ce1c
plnine <pl9-fund@pm.me>
2023-01-20 10:35:17 +0300

    Update update.sh

--- nodes/celestia/update.sh
+++ nodes/celestia/update.sh
@@ -11,9 +11,9 @@ printRed  =======================
 echo $(cyanprint    'Установлена версия')
 var1=`celestia-appd version`
 #var1=0.11.0
-var2=0.11.0
+var2="0.11.0"
 
-if [[ $var1 = $var2 ]]; then
+if [[ "$var1" = "$var2" ]]; then
 echo "OK"
 else 
 echo "upfate"
