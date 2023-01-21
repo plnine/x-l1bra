58824111b4a142bb58e89a49477cb78a40788728
plnine <pl9-fund@pm.me>
2023-01-22 00:38:20 +0300

    update

--- nodes/nibiru/install.sh
+++ nodes/nibiru/install.sh
@@ -54,12 +54,12 @@ read -r -p "Введите имя ноды:" NODE_MONIKER
 
 printBCyan "Пожалуйста подождите........" && sleep 1
 printYellow "1. Oбновляем наш сервер........" && sleep 1
-	sudo apt update  > /dev/null 2>&1 && sudo apt upgrade -y  > /dev/null 2>&1
+	sudo apt update  > /dev/null 
 printGreen "Готово!" && sleep 1
 
 
 printYellow "2. Устанавливаем дополнительные пакеты........" && sleep 1
-	sudo apt install -y make clang pkg-config libssl-dev build-essential git gcc lz4 chrony unzip curl jq ncdu htop net-tools lsof fail2ban wget -y > /dev/null 2>&1
+	sudo apt install curl git jq lz4 build-essential -y
 printGreen "Готово!" && sleep 1
 
 
@@ -79,95 +79,95 @@ printGreen "Готово!" && sleep 1
 
 
 printYellow "4. Устанавливаем go........" && sleep 1
-	if [ ! -f "/usr/local/go/bin/go" ]; then
-	bash <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-scripts/master/utils/go_install.sh")
-	source .bash_profile
-	fi
+	source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/go/go_1.19.4.sh)
 printGreen "Готово!" && sleep 1
 
 
 printYellow "5. Скачиваем и устанавливаем бинарник........"
-	cd || return
+	cd $HOME
 	rm -rf nibiru
-	git clone https://github.com/NibiruChain/nibiru
-	cd nibiru || return
-	git checkout v0.16.2
-	make install
-	nibid version
+	git clone https://github.com/NibiruChain/nibiru.git
+	cd nibiru
+	git checkout v0.16.3
+	make build
+	mkdir -p $HOME/.nibid/cosmovisor/genesis/bin
+	mv build/nibid $HOME/.nibid/cosmovisor/genesis/bin/
+	rm -rf build
+printGreen "Готово!" && sleep 1
+
+printYellow "Устанавливаем cosmovisor и создаем сервис........"
+	go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
+[Unit]
+Description=nibiru-testnet node service
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.nibid"
+Environment="DAEMON_NAME=nibid"
+Environment="UNSAFE_SKIP_BACKUP=true"
+[Install]
+WantedBy=multi-user.target
+EOF
+	sudo systemctl daemon-reload
+	sudo systemctl enable nibid
+	ln -s $HOME/.nibid/cosmovisor/genesis $HOME/.nibid/cosmovisor/current
+	sudo ln -s $HOME/.nibid/cosmovisor/current/bin/nibid /usr/local/bin/nibid
 printGreen "Готово!" && sleep 1
 
 
 printYellow "6. Инициализируем ноду........" && sleep 1
-	nibid config keyring-backend test
 	nibid config chain-id nibiru-testnet-2
-	nibid init $NODE_MONIKER --chain-id nibiru-testnet-2
+	nibid config keyring-backend test
+	nibid config node tcp://localhost:39657
+	nibid init $MONIKER --chain-id nibiru-testnet-2
 printGreen "Готово!" && sleep 1
 
 
 printYellow "7. Добавляем сиды и пиры........" && sleep 1
-	sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001unibi"|g' $HOME/.nibid/config/app.toml
-	seeds="dabcc13d6274f4dd86fd757c5c4a632f5062f817@seed-2.nibiru-testnet-2.nibiru.fi:26656,a5383b33a6086083a179f6de3c51434c5d81c69d@seed-1.nibiru-testnet-2.nibiru.fi:26656"
-	peers=""
-	sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/.nibid/config/config.toml
+	curl -Ls https://snapshots.kjnodes.com/nibiru-testnet/genesis.json > $HOME/.nibid/config/genesis.json
+	curl -Ls https://snapshots.kjnodes.com/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
+	sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@nibiru-testnet.rpc.kjnodes.com:39659\"|" $HOME/.nibid/config/config.toml
 printGreen "Готово!" && sleep 1
-
-##################
-curl -s https://rpc.testnet-2.nibiru.fi/genesis | jq -r .result.genesis > $HOME/.nibid/config/genesis.json
-sha256sum $HOME/.nibid/config/genesis.json
-curl -s https://snapshots3-testnet.nodejumper.io/nibiru-testnet/addrbook.json > $HOME/.nibid/config/addrbook.json
 
 
 printYellow "8. Настраиваем прунинг........" && sleep 1
-	sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/.nibid/config/app.toml
-	sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/.nibid/config/app.toml
-	sed -i 's|pruning-interval = "0"|pruning-interval = "17"|g' $HOME/.nibid/config/app.toml
+	pruning="custom"
+	pruning_keep_recent="1000"
+	pruning_interval="10"
+	sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.nibid/config/app.toml
+	sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.nibid/config/app.toml
+	sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.nibid/config/app.toml
 printGreen "Готово!" && sleep 1
 
 
 printYellow "9. Задаем минимальную цену за gas........" && sleep 1
-	sed -i.bak 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
+	sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.025unibi\"|" $HOME/.nibid/config/app.toml
 printGreen "Готово!" && sleep 1
 
 
-printYellow "10. Создаем сервис файл........" && sleep 1
-sudo tee /etc/systemd/system/nibid.service > /dev/null << EOF
-[Unit]
-Description=Nibiru Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which nibid) start
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-EOF
+printYellow "10. Выключаем индексацию........" && sleep 1
+indexer="null"
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.nibid/config/config.toml
 printGreen "Готово!" && sleep 1
 
 
-printYellow "10. Сбрасываем данные........"
-	nibid tendermint unsafe-reset-all --home $HOME/.nibid --keep-addr-book
+printYellow "10. Устанавливаем кастомные порты........"
+	sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:39658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:39657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:39060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:39656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":39660\"%" $HOME/.nibid/config/config.toml
+	sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:39317\"%; s%^address = \":8080\"%address = \":39080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:39090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:39091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:39545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:39546\"%" $HOME/.nibid/config/app.toml
 printGreen "Готово." && sleep 1
 
 
-printYellow "11. Подгружаем снапшот........"
-SNAP_RPC="https://nibiru-testnet.nodejumper.io:443"
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.nibid/config/config.toml
-curl https://snapshots3-testnet.nodejumper.io/nibiru-testnet/wasm.lz4 | lz4 -dc - | tar -xf - -C $HOME/.nibid/data
+printYellow "11. Подгружаем последний снапшот........"
+	curl -L https://snapshots.kjnodes.com/nibiru-testnet/snapshot_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.nibid
 printGreen "Готово."
 
 
-printYellow "11. Запускаем ноду........" && sleep 1
-	sudo systemctl daemon-reload
-	sudo systemctl enable nibid
+printYellow "11. Запускаем ноду........" && sleep 2
 	sudo systemctl start nibid
 printGreen "Готово!"
 
@@ -186,38 +186,37 @@ $(printGreen    'Установка завершена.') $(printGreenBlink '!!!
  		2) Проверить синхронизацию
  		3) В меню
 Нажмите Enter:  "
-   read -r ans
-    case $ans in
-        1)
-        subsubmenu
-        ;;
-        2)
-        curl -s localhost:26657/status | jq .result.sync_info.catching_up
-        submenu
-        ;;
-        3)
-        source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/main.sh)
-        ;;
-        *)
-        source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/logo.sh)
-        printRed  =======================================================================
-        echo $(redprint 'Неверный запрс !!!')
-        submenu
-        ;;
-    esac
+	read -r ans
+	case $ans in
+		1)
+		subsubmenu
+		;;
+		2)
+		curl -s localhost:26657/status | jq .result.sync_info.catching_up
+		submenu
+		;;
+		3)
+		source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/main.sh)
+		;;
+		*)
+		source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/scripts/logo.sh)
+		printRed  =======================================================================
+		echo $(redprint 'Неверный запрс !!!')
+		submenu
+		;;
+	esac
 }
 
 subsubmenu(){
-    echo -ne "
-$(printYellow    'Для того что бы остановить журнал логов надо нажать') $(printBCyan 'CTRL+Z') $(printYellow '!!!')
-
-$(printBCyan 'Для продолжения нажмите Enter:')  "
-	read -r ans
-	case $ans in
-		*)
-		sudo journalctl -u nibid -f --no-hostname -o cat
-		submenu
-		;;
+	echo -ne "
+	$(printYellow    'Для того что бы остановить журнал логов надо нажать') $(printBCyan 'CTRL+Z') $(printYellow '!!!')
+	$(printBCyan 'Для продолжения нажмите Enter:')  "
+		read -r ans
+		case $ans in
+			*)
+			sudo journalctl -u nibid -f --no-hostname -o cat
+			submenu
+			;;
 	esac
 }
 
--- scripts/go/go_1.19.4.sh
+++ scripts/go/go_1.19.4.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-while getopts v: flag; do
-  case "${flag}" in
-  v) VER=$OPTARG ;;
-  *) echo "WARN: unknown parameter: ${OPTARG}"
-  esac
-done
-
-version=${VER:-"1.19.4"}
-
-curl -L -# -O "https://golang.org/dl/go$version.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
-rm "go$version.linux-amd64.tar.gz"
-
-touch $HOME/.bash_profile
-source $HOME/.bash_profile
-PATH_INCLUDES_GO=$(grep "$HOME/go/bin" $HOME/.bash_profile)
-if [ -z "$PATH_INCLUDES_GO" ]; then
-  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
-  echo "export GOPATH=$HOME/go" >> $HOME/.bash_profile
-fi
+sudo curl -Ls https://golang.org/dl/go1.19.4.linux-amd64.tar.gz | sudo tar -C /usr/local -xz
+tee -a $HOME/.profile > /dev/null << EOF
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+source $HOME/.profile
