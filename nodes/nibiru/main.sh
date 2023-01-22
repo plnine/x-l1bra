7e0603d4f2142dc5e63c1baa7e82d4476a184f2c
plnine <pl9-fund@pm.me>
2023-01-22 03:04:37 +0300

    Update main.sh

--- nodes/nibiru/main.sh
+++ nodes/nibiru/main.sh
@@ -47,11 +47,11 @@ mainmenu() {
 }
 
 install(){
-source <(curl -s https://github.com/plnine/x-l1bra/blob/main/nodes/nibiru/install.sh)
+source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/install.sh)
 }
 
 control(){
-source <(curl -s https://github.com/plnine/x-l1bra/blob/main/nodes/nibiru/control.sh)
+source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/control.sh)
 }
 
 update(){
@@ -60,11 +60,11 @@ printLogo
 echo
 echo $(printBYellow "Coming soon !!!")
 mainmenu
-#source <(curl -s https://github.com/plnine/x-l1bra/blob/main/nodes/nibiru/update.sh)
+#source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/update.sh)
 }
 
 delet(){
-source <(curl -s https://github.com/plnine/x-l1bra/blob/main/nodes/nibiru/delet.sh)
+source <(curl -s https://raw.githubusercontent.com/plnine/x-l1bra/main/nodes/nibiru/delet.sh)
 }
 
 back(){
