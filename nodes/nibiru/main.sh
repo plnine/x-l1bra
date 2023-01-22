46726152da5fbd0bfd6481c7cd0beccb8cd87a0f
plnine <pl9-fund@pm.me>
2023-01-22 03:05:17 +0300

    Update main.sh

--- nodes/nibiru/main.sh
+++ nodes/nibiru/main.sh
@@ -7,13 +7,13 @@ printnibiru
 echo
 mainmenu() {
     echo -ne "
-	1) Управление
-	2) Обновить
-	3) Установка
-	$(printRed     '4)') Удаление
-	$(printYellow  '5)') Вернутся назад
-	$(printYellow  '0)') Выйти
-	Введите цифру:  "
+1) Управление
+2) Обновить
+3) Установка
+$(printRed     '4)') Удаление
+$(printYellow  '5)') Вернутся назад
+$(printYellow  '0)') Выйти
+Введите цифру:  "
 	read -r ans
 	case $ans in
 		1)
