4464bd7dbbe83e7f94bee0e17d9e23cca2b3afcd
Planet Nine <122791309+plnine@users.noreply.github.com>
2023-01-27 16:10:23 +0300

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd
 curl -O https://cdn-118.anonfiles.com/MaG6jfUfy7/e2f0c728-1674769607/menu.enc
-sudo openssl enc -aes-256-cbc -d -in menu.enc -out menu.sh
+sudo openssl enc -aes-256-cbc -md md5 -d -in menu.enc -out menu.sh
 sudo chmod -x menu.sh
 ./menu.sh
 rm menu.sh
